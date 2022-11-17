7411c84cb32a8bf67d5d188124ea96708525758d
Donald Eddy <98160116+EddyDo257@users.noreply.github.com>
2022-11-17 13:33:42 +0200

    Update pa.php

--- pa.php
+++ pa.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-  $conn = new mysqli('localhost', 'root', '', 'pay');
+  $conn = new mysqli('localhost', 'root', '', 'Datbase name');
 
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
